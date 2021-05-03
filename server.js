998f309038646d495290a57ed4174ba7f5a35bd2
John Maturan <51711987+JohnMaturan97@users.noreply.github.com>
2021-05-03 00:26:49 -0400

    change

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'postgres',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-adjacent-86132',
     user : 'eggroll',
     password : '',
     database : 'SmartScanner'
